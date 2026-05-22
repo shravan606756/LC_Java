5afb2710ca81c3acbc75d1ef8b50409aff15d898
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-05-22 10:35:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (63.65%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int search(int[] arr, int target) 
+    {
+        int n=arr.length;
+        int low=0 , high=n-1;
+
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            if(arr[mid]==target)
+            {
+                return mid;
+            }
+
+            if(arr[low]<=arr[mid])
+            {
+                if(target>=arr[low] && target<=arr[mid])
+                {
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+
+            else
+            {
+                if(target>=arr[mid] && arr[high]>=target)
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
