e2ba92a66c1a12832e9710def6c620a0bac326cd
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-06-03 20:33:34 +0530

    Time: 2 ms (100.00%) | Memory: 100.9 MB (17.24%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
@@ -1,25 +1,33 @@
-class Solution {
+class Solution 
+{
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) 
+    {
+        int min = Integer.MAX_VALUE;
 
-    private int findFinishTime(int[] start1, int[] duration1, int[] start2, int[] duration2) {
+        int f1 = giveFinishTime(landStartTime, landDuration, waterStartTime, waterDuration);
+        int f2 =  giveFinishTime(waterStartTime, waterDuration, landStartTime, landDuration);
 
-        int finish1 = Integer.MAX_VALUE;
-        for (int i = 0; i < start1.length; i++) {
-            finish1 = Math.min(finish1, start1[i] + duration1[i]);
-        }
+        min = Math.min(min, Math.min(f1, f2));
 
-        int finish2 = Integer.MAX_VALUE;
-        for (int i = 0; i < start2.length; i++) {
-            finish2 = Math.min(finish2, Math.max(finish1, start2[i]) + duration2[i]);
-        }
-
-        return finish2;
+        return min;
     }
 
-    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int pehleLand_FirWater = findFinishTime(landStartTime, landDuration, waterStartTime, waterDuration);
+    public int giveFinishTime(int stTime1[], int duration1[], int stTime2[], int duration2[])
+    {
+        int finishTime1=Integer.MAX_VALUE;
 
-        int pehleWater_FirLand = findFinishTime(waterStartTime, waterDuration, landStartTime, landDuration);
+        for(int i=0 ; i<stTime1.length ; i++)
+        {
+            finishTime1 = Math.min(finishTime1, stTime1[i]+duration1[i]);
+        }
 
-        return Math.min(pehleLand_FirWater, pehleWater_FirLand);
+        int finishTime2=Integer.MAX_VALUE;
+
+        for(int i=0 ; i<stTime2.length ; i++)
+        {
+            finishTime2 = Math.min(finishTime2, Math.max(finishTime1, stTime2[i]) + duration2[i]);
+        }
+
+        return finishTime2;
     }
 }
