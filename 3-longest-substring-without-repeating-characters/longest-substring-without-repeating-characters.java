d4fe8a2096789c395793ba5bc4af037b6def70ee
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-06-15 19:01:17 +0530

    Time: 8 ms (17.91%) | Memory: 46.1 MB (59.42%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,27 +3,19 @@ class Solution {
     {
         Map<Character, Integer> freq = new HashMap<>();
         int low=0, max = Integer.MIN_VALUE;
-        for(int high=0 ; high<s.length(); high ++)
-        {
+        for(int high=0 ; high<s.length(); high ++){
             freq.put(s.charAt(high), freq.getOrDefault(s.charAt(high),0)+1);
-
-            while(freq.get(s.charAt(high))>1)
-            {
+            while(freq.get(s.charAt(high))>1){
                 int count = freq.get(s.charAt(low));
                 count--;
                 if(count==0) freq.remove(s.charAt(low));
                 else freq.put(s.charAt(low),count);
-
                 low++;
             }
 
-            if(freq.get(s.charAt(high))==1)
-            {
-                int len = high-low+1;
-                max = Math.max(max, len);
-            }
+            int len = high-low+1;
+            max = Math.max(max, len);
         }
-
         return max==Integer.MIN_VALUE ? 0 : max;
     }
 }
