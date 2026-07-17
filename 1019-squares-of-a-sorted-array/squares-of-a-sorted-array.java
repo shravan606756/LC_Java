2d317ccd1574ca8840d4f37e9e59def34ad2f48e
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-17 21:26:04 +0530

    Time: 2 ms (45.23%) | Memory: 47.9 MB (18.36%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,13 +1,85 @@
 class Solution {
-    public int[] sortedSquares(int[] nums) {
-        int [] sq = new int[nums.length];
+    public int[] sortedSquares(int[] arr) 
+    {
+        int n = arr.length;
+        int idx=0;
 
-        for(int i=0 ; i<nums.length ; i++)
+        List<Integer> pos = new ArrayList<>();        
+        List<Integer> neg = new ArrayList<>();
+
+        int res[] = new int[n];
+
+        for (int i = 0; i < n; i++)
         {
-            sq[i] = nums[i]*nums[i];
+            if (arr[i] >= 0)
+                pos.add(arr[i]);
+            else
+                neg.add(arr[i]);
         }
 
-        Arrays.sort(sq);
-        return sq;
+        if(pos.size()==0){
+            for(int i=0;i<neg.size();i++)
+                res[i]=neg.get(i)*neg.get(i);
+            return rev(res);
+        }
+
+        if(neg.size()==0)
+        {
+            for(int i=0;i<pos.size();i++)
+                res[i]=pos.get(i)*pos.get(i);
+            return res;
+        }
+
+        int l=0 , r=neg.size()-1;
+        while(l<pos.size() && r>=0)
+        {
+            int a = pos.get(l)*pos.get(l);
+            int b = neg.get(r)*neg.get(r);
+
+            if(a>b)
+            {
+                res[idx++] = b;
+                r--;
+            }
+
+            else if(a<b)
+            {
+                res[idx++] = a;
+                l++;
+            }
+
+            else{
+                res[idx++] = a;
+                res[idx++] = b;
+                l++;
+                r--;
+            }
+        }
+
+        while(l<pos.size())
+        {
+            res[idx++] = pos.get(l)*pos.get(l);
+            l++;
+        }
+        while(r>=0)
+        {
+            res[idx++] = neg.get(r)*neg.get(r);
+            r--;
+        }
+
+        return res;
+    }
+
+    public int [] rev(int arr[])
+    {
+        int n = arr.length;
+        for(int i=0 ; i<n/2 ; i++)
+        {
+            int temp = arr[i];
+            arr[i] = arr[n-i-1];
+            arr[n-i-1] = temp;
+        }
+
+        return arr;
     }
 }
