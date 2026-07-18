24f7938bbe6d0fb18d2b8d16c2fed27521309e97
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-18 11:49:45 +0530

    Time: 16 ms (50.62%) | Memory: 42.6 MB (93.76%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.java
+++ 457-circular-array-loop/circular-array-loop.java
@@ -1,26 +1,36 @@
 class Solution {
     public boolean circularArrayLoop(int[] arr) 
     {
-        for(int i=0 ; i<arr.length ; i++)
+        int n = arr.length;
+
+        for(int i=0 ; i<n ; i++)
         {
-            int slow=i;
+            int slow = i;
             int fast = i;
-
-            int currDir = 0;
-
-            if(arr[i]>0) currDir = 1;
-            else currDir = -1;
+            int currDir = 1;
+            
+            if(arr[i]>0){
+                currDir = 1;
+            }else{
+                currDir = -1;
+            }
 
             while(true)
             {
-                slow = getNext(arr, slow, currDir, arr.length);
-                fast = getNext(arr, fast, currDir, arr.length);
+                slow = getNext(arr, currDir, slow, n);
+                fast = getNext(arr, currDir, fast, n);
 
-                if(fast!=-1){
-                    fast = getNext(arr, fast, currDir, arr.length);
-                }if(slow==-1 || fast==-1){
+                if(fast!=-1)
+                {
+                    fast = getNext(arr, currDir, fast, n);
+                }
+
+                if(slow==-1 || fast==-1)
+                {
                     break;
-                }if(fast==slow){
+                }
+
+                if(slow==fast){
                     return true;
                 }
             }
@@ -29,16 +39,20 @@ class Solution {
         return false;
     }
 
-    public int getNext(int arr[],int currIdx, int currDir, int n)
+    public int getNext(int arr[], int currDir, int currIdx, int n)
     {
-        int nextIdx = (currIdx + arr[currIdx]) %n;
-        int nextDir = 0;
-        if(nextIdx<0){
-            nextIdx= (nextIdx+n)%n;
+        int nextIdx = (currIdx + arr[currIdx]) % n;
+        if(nextIdx<0)
+        {
+            nextIdx = (nextIdx + n)%n; 
         }
 
-        if(arr[nextIdx]>0) nextDir = 1;
-        else nextDir = -1;
+        int nextDir = 0;
+        if(arr[nextIdx]>0){
+            nextDir = 1;
+        }else{
+            nextDir = -1;
+        }
 
         if(currDir!=nextDir || currIdx==nextIdx){
             return -1;
