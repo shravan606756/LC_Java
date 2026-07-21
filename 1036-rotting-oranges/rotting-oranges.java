702b8dd23417c8e3ec6a76ae3180829f47cd4211
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-07-21 11:03:15 +0530

    Time: 2 ms (91.02%) | Memory: 43.9 MB (85.36%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,83 +1,67 @@
-class Pair
-{
-    int n = 0 , m = 0;
-
-    Pair(int n , int m)
-    {
-        this.n = n;
-        this.m = m;
-    }
-}
-
 class Solution {
     public int orangesRotting(int[][] grid) 
     {
         int n = grid.length;
         int m = grid[0].length;
 
-        int x[] = {1,-1,0,0};
-        int y[] = {0,0,1,-1};
+        Queue<int []> q = new LinkedList<>();
+        int fresh=0, time=0;
 
-        int fresh=0 , time=0;
-        Queue <Pair> q = new LinkedList<>();
+        int x[] = {-1, 1, 0, 0};
+        int y[] = {0, 0, -1, 1};
+
         for(int i=0 ; i<n ; i++)
         {
             for(int j=0 ; j<m ; j++)
             {
                 if(grid[i][j]==2)
                 {
-                    q.offer(new Pair(i,j));
-                    grid[i][j] = -2;
+                    q.offer(new int[]{i, j});
+                    grid[i][j]=-2;
                 }
-                else if(grid[i][j]==1){
+
+                if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
 
-        while(!q.isEmpty() && fresh>0)
-        {
+        while(!q.isEmpty() && fresh>0){
             int size = q.size();
 
-            while(size>0)
-            {
-                Pair z = q.peek();
+            while(size>0){
+                int temp[] = q.peek();
                 q.poll();
 
-                int r = z.n;
-                int c = z.m;
+                int r = temp[0];
+                int c = temp[1];
 
                 for(int k=0 ; k<4 ; k++)
                 {
                     int row = r+x[k];
                     int col = c+y[k];
 
-                    if(isValid(row , col , n , m) && grid[row][col]==1)
-                    {
-                        q.offer(new Pair(row,col));
-                        grid[row][col]=-2;
+                    if(isValid(row, col, n, m) && grid[row][col]==1){
+                        q.offer(new int[]{row, col});
+                        grid[row][col]=2;
                         fresh--;
                     }
                 }
-
                 size--;
-            }
-
+            } 
             time++;
         }
 
         if(fresh>0) return -1;
-
         return time;
     }
 
-    public static boolean isValid(int i, int j, int n, int m)
+    public boolean isValid(int i, int j, int n, int m)
     {
-        if(i<0 || i>=n || j<0 || j>=m)
-        {
+        if(i<0 || i>=n || j<0 || j>=m){
             return false;
         }
-        
+
         return true;
     }
 }
