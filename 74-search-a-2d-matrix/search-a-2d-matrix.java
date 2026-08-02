ce134f065c7258d15358ee70a1a93c357e060da4
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-02 22:53:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (24.90%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,51 +1,24 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) 
-    {
-        /*
-            finding the appropiate row first
-            then treating that row as a individual arr
-            and doing bs to find target as mat is sorted
-        */
-
-        //finding row first
-
+    public boolean searchMatrix(int[][] matrix, int target){
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int low=0, high=n-1;
+        int low=0, high=(n*m)-1;
 
         while(low<=high)
         {
             int guess = low + (high-low)/2;
+            int row = guess/m;
+            int col = guess%m;
 
-            if(matrix[guess][0]>target){  
+            if(matrix[row][col]>target){
                 high = guess-1;
-            }else if(matrix[guess][m-1]<target){
-                low = guess+1;
-            }else{
-                return binarySearch(matrix[guess], target);
-            }
-        }
-
-        return false;
-    }
-
-    public boolean binarySearch(int arr[], int k)
-    {
-        int n  =arr.length;
-        int low = 0, high=n-1;
-
-        while(low<=high){
-            int guess = low + (high-low)/2;
-
-            if(arr[guess]>k){
-                high = guess-1;
-            }else if(arr[guess]<k){
+            }else if(matrix[row][col]<target){
                 low = guess+1;
             }else{
                 return true;
             }
-        }
+        }        
 
         return false;
     }
