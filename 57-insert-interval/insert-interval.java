91464455dde922bf81e0e0245c52e6026096953d
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-15 14:55:57 +0530

    Time: 4 ms (8.33%) | Memory: 47 MB (76.08%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,41 +1,49 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) 
     {   
-        List<int[]> newNiggaGroup = new ArrayList<>();
-        int n = intervals.length, i=0;
+        Arrays.sort(intervals, (x,y)->x[0]-y[0]);
+
+        List<int[]> arr = new ArrayList<>();
+        int n = intervals.length;
+        int idx=0;
+        while(idx<n && intervals[idx][0]<newInterval[0]){
+            arr.add(intervals[idx]);
+            idx++;
+        }
+
+        arr.add(newInterval);
         
-        while(i<n && intervals[i][0]<newInterval[0]){
-            newNiggaGroup.add(intervals[i]);
-            i++;
+        while(idx<n){
+            arr.add(intervals[idx]);
+            idx++;
         }
-        newNiggaGroup.add(newInterval);
-        while(i<n){
-            newNiggaGroup.add(intervals[i]);
-            i++;
-        }
-        int niggaArr[][] = newNiggaGroup.toArray(new int[newNiggaGroup.size()][]);
-        return mergeNiggers(niggaArr);
+
+        int [][] newArr = arr.toArray(new int[arr.size()][]);
+        return merge(newArr);
     }
 
-    public int [][] mergeNiggers(int [][]niggers)
-    {
-        List<int[]> resultNigga = new ArrayList<>();
-        int start1 = niggers[0][0];
-        int end1 = niggers[0][1];
-        
-        for(int i=1 ; i<niggers.length ; i++){
-            int start2 = niggers[i][0];
-            int end2 = niggers[i][1];
+    public int[][] merge(int arr[][]){
+        Arrays.sort(arr, (x, y) -> x[0]-y[0]);
+        List<int[]> res = new ArrayList<>();
 
-            if(end1>=start2){
+        int n = arr.length;
+        int start1 = arr[0][0];
+        int end1 = arr[0][1];
+
+        for(int i=1 ; i<n ; i++){
+            int start2 = arr[i][0];
+            int end2 = arr[i][1];
+
+            if(start2<=end1){
                 end1 = Math.max(end1, end2);
             }else{
-                resultNigga.add(new int[]{start1, end1});
+                res.add(new int[]{start1, end1});
                 start1 = start2;
                 end1 = end2;
             }
         }
-        resultNigga.add(new int[]{start1, end1});
-        return resultNigga.toArray(new int[resultNigga.size()][]);
+
+        res.add(new int[]{start1, end1});
+        return res.toArray(new int[res.size()][]);
     }
 }
