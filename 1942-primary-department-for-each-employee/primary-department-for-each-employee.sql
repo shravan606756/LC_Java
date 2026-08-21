89d73ef2b691839330eda1471e42a04a69461708
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-21 11:33:36 +0530

    Time: 601 ms (53.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+#if 1 then primary, else if 'y' then primary
+
+select distinct employee_id, department_id
+from employee
+where employee_id in(
+    select employee_id
+    from employee
+    group by employee_id
+    having count(employee_id)=1
+) or primary_flag='Y'
+order by employee_id
