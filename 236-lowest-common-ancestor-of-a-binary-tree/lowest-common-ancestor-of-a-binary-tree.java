d9a86bc43c77d8bb95942f3589ee9de8e80ce2e5
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-25 16:12:46 +0530

    Time: 16 ms (52.52%) | Memory: 70 MB (35.24%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,19 +8,18 @@ class Solution {
 
     public int lca(TreeNode root, TreeNode p, TreeNode q)
     {
-        if(root==null){
-            return 0;
-        }
+        if(root==null) return 0;
 
         int left = lca(root.left, p, q);
         int right = lca(root.right, p, q);
-        int self = 0;
+        int self=0;
 
         if(root==p || root==q){
             self=1;
         }
 
-        int total = left+right+self;
+        int total =  left+right+self;
+
         if(total==2 && ans==null){
             ans=root;
         }
