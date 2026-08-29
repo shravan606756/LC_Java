2484e38166ec06d834060e8316345beb05b219f8
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-08-29 22:35:44 +0530

    Time: 1 ms (97.49%) | Memory: 46.6 MB (14.58%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,34 +1,30 @@
 class Solution {
+    int idx=0;
+    Map<Integer, Integer> map = new HashMap<>();
+
     public TreeNode buildTree(int[] preorder, int[] inorder) 
     {
         int n = preorder.length;
 
-        int idx[] = new int[1];
-
-        HashMap <Integer , Integer> map = new HashMap<>();
-
-        for(int i=0 ; i<inorder.length ; i++)
-        {
-            map.put(inorder[i] , i);
+        for(int i=0 ; i<inorder.length ; i++){
+            map.put(inorder[i], i);
         }
 
-        return func(preorder , inorder , idx , map , 0 , n-1);
+        return sol(preorder, 0, n-1);
     }
 
-    public TreeNode func(int preorder[] , int inorder[] , int idx[] , HashMap<Integer , Integer> map , int low , int high )
-    {
-        if(low>high)
-        {
+    public TreeNode sol(int[] preorder, int low, int high){
+        if(low>high){
             return null;
         }
-        
-        int rootVal = preorder[idx[0]];
-        TreeNode root = new TreeNode(rootVal);
-        int id = map.get(rootVal);
-        idx[0]++;
 
-        root.left = func(preorder , inorder , idx , map , low , id-1);
-        root.right = func(preorder , inorder , idx , map , id+1 , high);
+        int rootVal = preorder[idx++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int id = map.get(rootVal);
+
+        root.left = sol(preorder, low, id-1);
+        root.right = sol(preorder, id+1, high);
 
         return root;
     }
