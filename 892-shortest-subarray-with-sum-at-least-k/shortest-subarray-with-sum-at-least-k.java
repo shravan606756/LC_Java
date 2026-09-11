fa41184c87cb31e808e4465ab4aedd8604b7c430
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-09-12 01:20:21 +0530

    Time: 27 ms (18.79%) | Memory: 60.9 MB (85.19%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.java
@@ -1,39 +1,36 @@
 class Solution {
     public int shortestSubarray(int[] arr, int k) 
     {
-        Deque<Integer> deq = new LinkedList<>();
-        int prefix[] = new int[arr.length];
+        int n = arr.length;
+        int prefix[] = new int[n];
+        Deque<Integer> deq = new ArrayDeque<>();
+        int minLength = Integer.MAX_VALUE, i=0;
 
-        int low=0, res = Integer.MAX_VALUE, i=0;
-
-        while(i<arr.length)
+        while(i<n)
         {
             if(i==0){
                 prefix[i] = arr[0];
+            }else{
+                prefix[i] = prefix[i-1] + arr[i];
             }
 
-            else prefix[i] = prefix[i-1]+arr[i];
-
-            if(prefix[i]>=k){
-                res = Math.min(res, i+1);
+            if (prefix[i] >= k) {
+                minLength = Math.min(minLength, i + 1);
             }
 
-            //shrinking
-            while(!deq.isEmpty() && prefix[i]-prefix[deq.peekFirst()]>=k)
-            {
-                res = Math.min(res, i-deq.peekFirst());
+            while(!deq.isEmpty() && prefix[i]-prefix[deq.peekFirst()]>=k){
+                minLength = Math.min(minLength, i - deq.peekFirst());
                 deq.pollFirst();
             }
 
-            //maintaing monotonic behaviour
             while(!deq.isEmpty() && prefix[i]<=prefix[deq.peekLast()]){
                 deq.pollLast();
             }
 
-            deq.offerLast(i);
+            deq.offer(i);
             i++;
-        }   
+        }
 
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return minLength==Integer.MAX_VALUE  ? -1 : minLength;
     }
 }
