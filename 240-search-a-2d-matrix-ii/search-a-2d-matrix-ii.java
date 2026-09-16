d49dd8eb4c34859b7ebc6b29c3cd3a45a97bb2e9
Shravan Singh Udawat <shravanudawat33@gmail.com>
2026-09-16 18:55:19 +0530

    Time: 3 ms (85.19%) | Memory: 48.4 MB (26.24%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -2,33 +2,21 @@ import java.util.Arrays;
 
 class Solution {
     public boolean searchMatrix(int[][] arr, int target) {
-        if (arr == null || arr.length == 0 || arr[0].length == 0) return false;
-        
         int n = arr.length;
         int m = arr[0].length;
-        int[] flat = new int[n * m];
         
-        int k = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                flat[k++] = arr[i][j];
-            }
-        }
-        
-        Arrays.sort(flat);
-        
-        int low = 0, high = flat.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (flat[mid] == target) {
+        int row=0, col=m-1;
+
+        while(row<n && col>=0){
+            if(arr[row][col]==target){
                 return true;
-            } else if (flat[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            }else if(arr[row][col]>target){
+                col--;
+            }else{
+                row++;
             }
         }
-        
+
         return false;
     }
 }
